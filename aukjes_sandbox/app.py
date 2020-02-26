987437ef0b247eb8b833d571ab4abceaced1dc2a
DutchDS <dutchdatasmarts@gmail.com>
2020-02-26 12:26:40 -0600

    chart working with correct json method now

--- aukjes_sandbox/app.py
+++ aukjes_sandbox/app.py
@@ -9,6 +9,8 @@ from flask import (
 import pandas as pd
 from sqlalchemy import create_engine
 from flask_sqlalchemy import SQLAlchemy
+import decimal
+import flask.json
 
 app = Flask(__name__)
 
@@ -60,9 +62,40 @@ def bar_china():
         query_text = query_text + text
         
     print(query_text)
+
     df_query = pd.read_sql_query(query_text, con=engine)
     bar_data = df_query.to_json()
     return bar_data
+
+@app.route("/api/bar_china")
+def bar_chart_china():
+    
+    connection_string = "postgres:postgres@localhost:5432/corona_db"
+    engine = create_engine(f'postgresql://{connection_string}')
+    # Read
+    result_set = []
+    result_set = engine.execute("select date, \
+        sum(conf_count) confirmed, \
+        sum(cured_count) cured, \
+        sum(dead_count) dead, \
+        sum(susp_count) suspected \
+        from daily_stats \
+        group by date \
+        order by date")  
+
+    
+    all_results = []
+    for date, conf_count, cured_count, dead_count, susp_count in result_set:
+        results_dict = {}
+        results_dict["date"] = date
+        results_dict["conf_count"] = conf_count
+        results_dict["cured_count"] = cured_count
+        results_dict["dead_count"] = dead_count
+        results_dict["susp_count"] = susp_count
+        all_results.append(results_dict)
+
+    return jsonify(all_results)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
