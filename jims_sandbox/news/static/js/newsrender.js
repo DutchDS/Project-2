71f529470dedbc02dff8093c89f2183c62e0077e
JimCDabc <jim.comas@gmail.com>
2020-02-28 16:45:22 -0600

    fixed asynch issue with renderNewsBanner

--- jims_sandbox/news/static/js/newsrender.js
+++ jims_sandbox/news/static/js/newsrender.js
@@ -1,13 +1,20 @@
 
-//
+// =============================================
+//  function to buled the news table banner
+//  
 function buildNewsTableBanner(dateStr, source) {
+  console.log(`------ Enter buildNewsBanner datStr=${dateStr}, source=${source} -----`)
   var banner = d3.select("#news-banner");
   console.log("buildNewsTableBanner:", banner);
   banner
     .html("")
     .text(`Coronavirus news from ${source} for ${dateStr}`);
+
+    console.log(`------ Exit buildNewsBanner datStr=${dateStr}, source=${source} -----`)
 }
 
+// =============================================
+// build the news table header
 function buildNewsTableHeader() {
 
   var thead = d3.select("#news-header");
@@ -33,8 +40,9 @@ function buildNewsTableHeader() {
     .text("Image")
 }
 
-
-// function loop through each report and add to the table
+// =============================================
+// function to build the news table body
+// loop through each report and add to the table
 function buildNewsTableBody(articles) {
   // Get a reference to the table body
   var tbody = d3.select("#news-body");
@@ -73,12 +81,14 @@ function buildNewsTableBody(articles) {
   });
 }
 
-// TEST Call to a newsapi json file to buils the HTML table for news.
-// This will be replaced by a Date change event listener and read of the right json file for the date
-// May also add selector for NEWS Source to listen for.
-function renderNewsForDate(dateStr, newsSource) {
+// =============================================
+// function to render the news table body for a  specific date and news source
+// reads file with specified path and name format
+// filepath = ""./static/newsdata/<date>/<newsSourceID>_<dateStr>.json"
+// example = "./static/newsdata/2020-02-24/bbc-news_2020-02-24.json"
+function renderNewsForDate(dateStr, newsSourceId) {
   newsdirPath  = `/static/newsdata/${dateStr}/`;
-  newsfileName = `${newsSource}_${dateStr}.json`;
+  newsfileName = `${newsSourceId}_${dateStr}.json`;
   newsfilePath = newsdirPath + newsfileName
   console.log("News File: ", newsfilePath);
 
@@ -88,41 +98,51 @@ function renderNewsForDate(dateStr, newsSource) {
   });
 }
 
+// =============================================
+// render the news banner with dateStr and news source name info
 var newsSources = []
 function renderNewsBanner(dateStr, newsSourceId) {
-  newsapiSrcs = `/static/newsdata/newsapi_sources.json`;
-  console.log("News Source names file: ", newsapiSrcs);
-  var sourceName = newsSourceId;
+  newsapi_source_json = `/static/newsdata/newsapi_sources.json`;
 
+  console.log("------ Enter renderNewsBanner -----")
+  console.log("News Source names file: ", newsapi_source_json);
+  console.log("News Sources array (1): ", newsSources);
+ 
+  // if all else fails just set the make the news sourceName the same as newsSourceID
+  var sourceName = newsSourceId;
+ 
   //convert news source id to a readable name
   if (!Array.isArray(newsSources) || !newsSources.length) {
     // newsSources array does not exist, is not an array, or is empty then 
     //  read json data for source metadata
-    d3.json(newsapiSrcs).then(function(newsSrcs) {
+    console.log("*** news source array is empty"); 
+    
+    d3.json(newsapi_source_json ).then(function(newsSrcs) {
       console.log(newsSrcs);
       newsSources = newsSrcs.sources;
+      console.log("updated newsSources array (2): ", newsSources);
+
       result = newsSources.find(source => source.id === newsSourceId);
       if (result) { sourceName = result.name }
+
+      // build the table banner with the dates tring and news source name
+      buildNewsTableBanner(dateStr, sourceName);
     }); 
   } else {
     // newsSources has already been read in
+    // convert the id to the name
     result = newsSources.find(source => source.id === newsSourceId);
+    if (result) { sourceName = result.name }
+    // build the table banner with the dates tring and news source name
+    buildNewsTableBanner(dateStr, sourceName);
   }
+  console.log("Current newsSources array (3): ", newsSources)
 
-  // build the table banner with the datestring and news source name
-  buildNewsTableBanner(dateStr, sourceName);
+  console.log("------ Exit renderNewsBanner -----")
 }
 
 // =============================================
-// Main  Set up
-newsDate = "2020-02-27"
-newsSrc = "bbc-news"
-buildNewsTableHeader();
-
-// Wrap this with an on Date Input (or Date Change) listener
-renderNewsBanner(newsDate, newsSrc);
-renderNewsForDate(newsDate, newsSrc);
-
+// listeners for date or news source selector change event
 d3.select("#corona-date").on("change", function() {
   newsDate = this.value
   renderNewsBanner(newsDate, newsSrc);
@@ -135,114 +155,15 @@ d3.select("#news_source").on("change", function() {
   renderNewsForDate(newsDate, newsSrc);
 });
 
-// DELETE THE REST OF THIS CODE AFTER FIGURE OUT HOW TO listen for a date change
+// =============================================
+// Initalize the news table on entry
+newsDate = "2020-02-27"
+newsSrc = "bbc-news"
 
-// From UFO Project....
-// // function loop through each report and add to the table
-// function buildHTMLTable(reports) {
-//   // Get a reference to the table body
-//   var tbody = d3.select("tbody");
-//   // Clear table body
-//   tbody.html("")
+function initializeNewsTable(){
+  buildNewsTableHeader();
+  renderNewsBanner(newsDate, newsSrc);
+  renderNewsForDate(newsDate, newsSrc);
+} 
 
-//   // loop through ech report and add to HTML table body
-//   reports.forEach((report) => {
-//     // console.log(report)
-//     var row = tbody.append("tr");
-//     Object.entries(report).forEach(([key, value]) => {
-//       var cell = row.append("td");
-//       cell.text(value);
-//     });
-//   });
-// }
-
-// // build initial table from the data
-// buildHTMLTable(tableData);
-
-
-// // JavaScript code so the user can to set multiple filters and search for UFO sightings 
-// // using the following criteria based on the table columns:
-// // date/time
-// // city
-// // state
-// // country
-// // shape
-
-// function filterbyDate(dataset, inDateStr) {
-//   // initialize filterdData array
-//   var filteredData = [];
-
-//   // parse the date and check to see if valid date
-//   var parseDate = d3.timeParse("%m/%d/%Y");
-//   var inputDate = parseDate(inDateStr);
- 
-//   if (inputDate !== null) {
-//     console.log(`${inDateStr} parsed to datetime: ${inputDate}`);
-
-//     // filter table by date-time that is taken from inputvalue.
-//     // var filteredData = tableData.filter(report => (parseDate(report.datetime) == inputDate));
-//     filteredData = 
-//       dataset.filter(report => (parseDate(report.datetime).getTime() === inputDate.getTime()));
-
-//     // console.log(filteredData);
-    
-//   } else {
-//     console.log(`INPUT ERROR: Invalid date entered: ${inDateStr}`)
-//   }
-
-//   return filteredData
-// }
-
-// // Select the button
-// var button = d3.select("#filter-btn");
-
-// button.on("click", function() {
-
-//   // initialize the result data to be the complete table.  then filter down
-//   var results= tableData;
-  
-//   // Get the value property of the input datetime,
-//   // Filter by Date if present
-//   // city, state, country, shape elements
-//   var inDateStr = d3.select("#datetime").property("value");
-//   if (inDateStr !== "") {
-//     console.log(`Filter by date: [${inDateStr}]`)
-//     results = filterbyDate(results, inDateStr);
-//   }
-
-//   // Get the value property of the input city,
-//   // Filter by city if present
-//   var inCity = d3.select("#city").property("value").toLowerCase();
-//   if (inCity !== "") {
-//     console.log(`Filter by city: [${inCity}]`)
-//     results = results.filter(report => (report.city === inCity)); 
-//   }
-
-//   // Get the value property of the input state,
-//   // Filter by state if present
-//   var inState = d3.select("#state").property("value").toLowerCase();
-//   if (inState !== "") {
-//     console.log(`Filter by state: [${inState}]`)
-//     results = results.filter(report => (report.state === inState)); 
-//   } 
-
-//   // Get the value property of the input country,
-//   // Filter by country if present
-//   var inCountry = d3.select("#country").property("value");
-//   if (inCountry !== "") {
-//     console.log(`Filter by country: [${inCountry}]`)
-//     results = results.filter(report => (report.country === inCountry)); 
-//   } 
-
-//   // Get the value property of the input datetime shape,
-//   // Filter by shape if present
-//   var inShape = d3.select("#shape").property("value");
-//   if (inShape !== "") {
-//     console.log(`Filter by shape: [${inShape}]`)
-//     results = results.filter(report => (report.shape === inShape)); 
-//   } 
-
-//   // build the HTML for resulting filtered table  
-//   buildHTMLTable(results);
-
-// });
+initializeNewsTable();
