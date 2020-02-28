8d752399eeda7e806a9939cca763f8673e826697
DutchDS <dutchdatasmarts@gmail.com>
2020-02-28 06:46:54 -0600

    world working

--- app.py
+++ app.py
@@ -16,8 +16,7 @@ app = Flask(__name__)
 
 DATABASE_URI = os.environ.get('DATABASE_URL', '') or "postgresql://postgres:postgres@localhost:5432/corona_db"
 print(DATABASE_URI)
-# connection_string = "postgres:postgres@localhost:5432/corona_db"
-# connection_string = 'jhmhjoanyrfkil:9cd0395b06b90f91ed2ea452bb2049b1d69921df39d0dbcfe18b54543f5e5e2c@ec2-50-17-178-87.compute-1.amazonaws.com:5432/decdba37fkfjcs'
+
 engine = create_engine(DATABASE_URI)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI 
@@ -26,18 +25,45 @@ db = SQLAlchemy(app)
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "postgresql://postgres:postgres@localhost:5432/corona_db"
 
 # db = SQLAlchemy(app)
-
 @app.route("/")
-def home(): 
-    mars_data = ['aukje', 'jim', 'vamsi','meliha']
-    return render_template("china.html", mars_data=mars_data)
+def world():
+    result_set = []
+    
+    query_str = open('static/sql/world_top_10.sql')
+    query_text = ""
+    
+    for text in query_str:
+        query_text = query_text + text
+        
+    result_set = engine.execute(query_text)
+
+    result_sum = []
+
+    query_str = open('static/sql/world_summary.sql')
+    query_text = ""
+    
+    for text in query_str:
+        query_text = query_text + text
+
+    result_sum = engine.execute(query_text)
+  
+    return render_template("world.html", world_facts=result_set, world_sum=result_sum)
+
+@app.route("/china")
+def china(): 
+    return render_template("china.html")
+
+@app.route("/slider")
+def slider(): 
+    return render_template("slider.html")
+
+@app.route("/news")
+def news():
+    return render_template("news.html")
 
 @app.route("/api/bar_china")
 def bar_chart_china():
     
-    # connection_string = "postgres:postgres@localhost:5432/corona_db"
-    # engine = create_engine(f'postgresql://{connection_string}')
-    # Read
     result_set = []
     result_set = engine.execute("select date, \
         sum(conf_count) confirmed, \
@@ -61,31 +87,29 @@ def bar_chart_china():
 
     return jsonify(all_results)
 
-@app.route("/world")
-def world():
-    mars_data = ['this', 'is', 'the','world']
-    return render_template("world.html", mars_data=mars_data)
+@app.route("/api/bar_world")
+def bar_chart_world():
+    
+    result_set = []
+    
+    query_str = open('static/sql/world_bar.sql')
+    query_text = ""
+    
+    for text in query_str:
+        query_text = query_text + text
+        
+    result_set = engine.execute(query_text)
+    
+    all_results = []
+    for date, conf_count, cured_count, dead_count in result_set:
+        results_dict = {}
+        results_dict["date"] = date
+        results_dict["conf_count"] = conf_count
+        results_dict["cured_count"] = cured_count
+        results_dict["dead_count"] = dead_count
+        all_results.append(results_dict)
+
+    return jsonify(all_results)
 
 if __name__ == "__main__":
     app.run(debug=True)
-####################################
-# OLD WAY OF RETRIEVING JSON FILE
-#####################################
-# @app.route("/api/bar_china/file.json")
-# def bar_china():
-
-#     connection_string = "postgres:postgres@localhost:5432/corona_db"
-#     engine = create_engine(f'postgresql://{connection_string}')
-
-#     query_str = open('static/sql/test_query.sql')
-#     query_text = ""
-#     for text in query_str:
-#         query_text = query_text + text
-        
-#     print(query_text)
-
-#     df_query = pd.read_sql_query(query_text, con=engine)
-#     bar_data = df_query.to_json()
-#     return bar_data
-
-
