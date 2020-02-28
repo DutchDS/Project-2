1257ff8f49a4f6adf2692b34fd6d113c99f5acc7
DutchDS <dutchdatasmarts@gmail.com>
2020-02-28 15:20:57 -0600

    make bardropdown work

--- static/js/world_bar.js
+++ static/js/world_bar.js
@@ -53,10 +53,10 @@ function create_chart(bar_data) {
         bar_dead.push(bar_data[i].dead_count)
     }
 
-    console.log(bar_dates)
-    console.log(bar_confirmed)
-    console.log(bar_cured)
-    console.log(bar_dead)
+    // console.log(bar_dates)
+    // console.log(bar_confirmed)
+    // console.log(bar_cured)
+    // console.log(bar_dead)
 
 
     x_values = bar_dates
