23537985af5c982c1830dd59e7fce1163f49a58b
DutchDS <dutchdatasmarts@gmail.com>
2020-02-28 19:00:55 -0600

    latest version

--- aukjes_sandbox/static/sql/china_bar.sql
+++
@@ -1,8 +0,0 @@
-select date, 
-	sum(conf_count)-sum(cured_count)-sum(dead_count) positive, 
-	sum(cured_count) cured, 
-	sum(dead_count) dead ,
-	sum(susp_count) suspected
-    from daily_stats
-    group by date
-    order by date  
--- aukjes_sandbox/static/sql/world_bar.sql
+++
@@ -1,9 +0,0 @@
-select date, 
-	country,
-	sum(conf_count)-sum(cured_count)-sum(dead_count) positive, 
-	sum(cured_count) cured, 
-	sum(dead_count) dead 
-    from daily_stats_world 
-    where country <> 'China' 
-    group by date, country
-    order by date, country  
