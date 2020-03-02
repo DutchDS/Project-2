0c6aa790e3527a79102e84795c34f5f6ed8f3040
DutchDS <dutchdatasmarts@gmail.com>
2020-03-02 12:28:36 -0600

    cleaned up world.js code and added comments

--- static/js/world.js
+++ static/js/world.js
@@ -1,23 +1,26 @@
+////////////////////////////////////////////////////////////
+////////////// Set All Variables ///////////////////////////
+////////////////////////////////////////////////////////////
+
 let slider_input = document.querySelector('input');                                                                    //assign the input from slider to variable a                                                                //assign the output under the slider to variable b
-
 var last_DB_date = $("#selectDate").text();
-console.log(last_DB_date)
-
-// Creating map object
 var myMap = L.map("map", {
   center: [34.5133, -10.1629],
   zoom: 2
 });
-// Adding tile layer
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.satellite",
-  accessToken: API_KEY
-}).addTo(myMap);
-
+var today = new Date()
+var yesterday =  today.setDate(today.getDate() - 1);
 var current_date = new Date('2020-01-23');
+var lastgeoUrl = "static/world_geojsons/" + last_DB_date + ".json";
+var geojson;
+var legend;
 
+
+////////////////////////////////////////////////////////////
+////////////// Define All Functions ////////////////////////
+////////////////////////////////////////////////////////////
+
+/// Function formatDate: Translate any date into a given string format ///
 function formatDate(date) {
   var d = new Date(date),
       month = '' + (d.getMonth() + 1),
@@ -32,38 +35,29 @@ function formatDate(date) {
   return [year, month, day].join('-');
 }
 
+/// Function task: Build in a delay per map to give it time to load and complete before the next one starts  ///
 function task(i) { 
   setTimeout(function() {
-
     script_date = formatDate(current_date)
-    // document.getElementById('selectDate').innerHTML = ""
-    // document.getElementById('selectDate').innerHTML = script_date 
     console.log(script_date)
+
     document.getElementById("chartDate").innerHTML = "Chart Date:  " + script_date
     geoUrl = "static/world_geojsons/" + script_date + ".json";
-    // get_new_layer()
-    // console.log(current_date)
+    
     console.log(geoUrl)
-  // call run
+ 
     get_new_layers(geoUrl);
   
-    current_date.setDate(current_date.getDate() + 1);
-    // current_date = current_date.setDate(current_date + 1);
-    
+    current_date.setDate(current_date.getDate() + 1);   
     }  
   , 500 * i); 
 } 
 
-var geojson;
-var legend;
+/// Function 3: Colors used by map - need to match legend ///
 function chooseColor(x) {
-  // console.log("this is line 57")
-  // console.log("--------------------------")
     if (x == 0)
-      { color = "#white";
-      opacity = .1 }
-      // color = "transparent"
-    else if (x <= 5)
+      color = "transparent"
+    else if (x >= 1 & x <= 5)
       color = "#f7a1f3"
     else if (x <= 50)
       color = "#f15be9"
@@ -73,16 +67,13 @@ function chooseColor(x) {
       color = "#a40e9d"
     else
       color = "#5e085a";
-    // console.log(color)
-
   return (color)
   }
 
+// Function get_one_layer used to retrieve and plot one map chosen by slider date (all exisiting polylines need to be cleared) ///
 function get_one_layer(geoUrl) { 
 
   d3.json(geoUrl).then(function(data) { 
-    // console.log("GEOJSON ", data)
-    // Create a new choropleth layer
     console.log("Plotting new layer")
 
     clear_polyline()
@@ -91,7 +82,6 @@ function get_one_layer(geoUrl) {
       style: function(feature) {
         return {
           color: "white",
-          // fillColor: "red",
           fillColor: chooseColor(feature.properties.confirmedCount), 
           fillOpacity: 0.85,
           weight: 0.5
@@ -102,7 +92,6 @@ function get_one_layer(geoUrl) {
     onEachFeature: function(feature, layer) {
       layer.bindPopup("<strong> " + feature.properties.american_name + "</strong>"
                 + "<hr><strong>Confirmed Cases: </strong>" + feature.properties.confirmedCount 
-                // + "<br><strong>Suspected Cases: </strong>" + feature.properties.suspectedCount 
                 + "<br><strong>Cured Cases: </strong>" + feature.properties.curedCount 
                 + "<br><strong>Dead Cases: </strong>" + feature.properties.deadCount
       );
@@ -111,29 +100,25 @@ function get_one_layer(geoUrl) {
 });
 }
 
+/// Function get new layers: used when iterating over dates and yielding maps. Called by reload_maps()
 function get_new_layers(geoUrl) { 
 
   d3.json(geoUrl).then(function(data) { 
-    // console.log("GEOJSON ", data)
-    // Create a new choropleth layer
     console.log("Plotting new layer")
 
     geojson = L.geoJson(data, {
       style: function(feature) {
         return {
           color: "white",
-          // fillColor: "red",
           fillColor: chooseColor(feature.properties.confirmedCount), 
           fillOpacity: 0.85,
           weight: 0.5
         }
       },
 
-    // Binding a pop-up to each layer
     onEachFeature: function(feature, layer) {
       layer.bindPopup("<strong> " + feature.properties.american_name + "</strong>"
                 + "<hr><strong>Confirmed Cases: </strong>" + feature.properties.confirmedCount 
-                // + "<br><strong>Suspected Cases: </strong>" + feature.properties.suspectedCount 
                 + "<br><strong>Cured Cases: </strong>" + feature.properties.curedCount 
                 + "<br><strong>Dead Cases: </strong>" + feature.properties.deadCount
       );
@@ -142,13 +127,12 @@ function get_new_layers(geoUrl) {
 });
 }
 
-
+/// FUnction create_legend: used to plot the legend in the map. Colors and ranges need to match function chooseColor
 function create_legend() {
     var legend = L.control({ position: "bottomleft" });
     legend.onAdd = function(map) {
       var div = L.DomUtil.create("div", "legend");
       div.innerHTML += "<h4>Corona Cases</h4>";
-      div.innerHTML += '<i style="background: #fde8fc"></i><span>0</span><br>';
       div.innerHTML += '<i style="background: #f7a1f3"></i><span>1 - 5</span><br>';
       div.innerHTML += '<i style="background: #f15be9"></i><span>6 - 50</span><br>';
       div.innerHTML += '<i style="background: #eb14e0"></i><span>51 - 500</span><br>';
@@ -161,18 +145,7 @@ function create_legend() {
 legend.addTo(myMap);
 }
 
-var today = new Date()
-var yesterday =  today.setDate(today.getDate() - 1);
-
-// initgeoUrl = "static/world_geojsons/" + formatDate(yesterday) + ".json";
-lastgeoUrl = "static/world_geojsons/" + last_DB_date + ".json";
-
-// console.log(initgeoUrl)
-
-get_new_layers(lastgeoUrl);
-create_legend()
-// reload()
-
+/// Function reload. Called by button Show Spread. Used to animate virus spread
 function reload() {
   day_count = today - current_date
   counter = Math.ceil((day_count/24/60/60/1000/ + 1))
@@ -180,12 +153,8 @@ function reload() {
     task(i); 
     } 
   }
-  
-d3.select("#selectAll").on("click", function() {
-    firstgeoUrl = "static/world_geojsons/" + current_date + ".json";
-    clear_polyline()
-    reload()});
 
+/// FUnction clear_polyline: clears any exisiting polylines on map that exist  
 function clear_polyline() {
   myMap.eachLayer(function (layer) {
     myMap.removeLayer(layer)
@@ -196,9 +165,34 @@ function clear_polyline() {
       id: "mapbox.satellite",
       accessToken: API_KEY
     }).addTo(myMap);
-} 
+  } 
 
-  slider_input.addEventListener('change', function () {                                                                   //using event listener with input gives instant response; use 'change' instead to see the difference in response
+////////////////////////////////////////////////////////////
+////////////// Execution Part  /////////////////////////////
+////////////////////////////////////////////////////////////
+
+/// Plot initial map layer /////////////////////////////////
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.satellite",
+    accessToken: API_KEY
+  }).addTo(myMap);
+    
+/// Add legend to the initial map //////////////////////////
+create_legend()
+
+/// Initial map retrieves the latest map in static/geojsons_world //////  
+get_new_layers(lastgeoUrl);
+
+/// Button Show Spread starts run of virus spread /////
+d3.select("#selectAll").on("click", function() {
+    // firstgeoUrl = "static/world_geojsons/" + current_date + ".json";
+  clear_polyline()
+  reload()});
+
+/// Slider input adjusts map with matching geojson ////  
+slider_input.addEventListener('change', function () {                                                                   //using event listener with input gives instant response; use 'change' instead to see the difference in response
     var day_one;                                                                                            //declare our day_one variable which will represent the day one of outbreak
     day_one = moment("22/01/2020", "DD/MM/YYYY");                                                           //assigning our day one value 27 Jan 2020 to day_one using moment lib
     var slider_day = moment(day_one,"DD/MM/YYYY").add((slider_input.value-1),'day');                                   //manipulating the slider input with day_one value using moment, to get date for slider input
@@ -209,4 +203,4 @@ function clear_polyline() {
     console.log(geoUrl)
     document.getElementById("chartDate").innerHTML = "Chart Date:  " + get_date
     get_one_layer(geoUrl);
-}, false);
+  }, false);
